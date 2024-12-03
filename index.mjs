0385338fa1bc5ae9a99e27c32fc579d3baa0878e
Tytan <122774874+ThomNardou@users.noreply.github.com>
2024-12-03 20:45:03 +0100

    feat(backend): add link to turn on/off the light

--- index.mjs
+++ index.mjs
@@ -2,29 +2,28 @@ import express from "express";
 import mqtt from "mqtt";
 
 const client = mqtt.connect("mqtt://blue.section-inf.ch:1883", {
-    username: "",
-    password: ""
+  username: "",
+  password: ""
 });
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<a href='http://localhost:3000/arroseur/on'>Turn on the light</a> <a href='http://localhost:3000/arroseur/off'>Turn off the light</a>");
 });
 
-client.on("connect", () => {
-    client.subscribe("arduinoTest", (err) => {
-      if (!err) {
-        client.publish("arduinoTest", "Hello mqtt");
-      }
-    });
-  });
+app.get("/arroseur/on", (req, res) => {
+  client.publish("arroseur", JSON.stringify({ status: "start" }));
+  res.send("The arroseur is on </br> <a href='http://localhost:3000/arroseur/off'>Turn off the light</a>");
 
-  client.on("message", (topic, message) => {
-    console.log(message.toString());
-    client.end();
-  })
+});
+
+app.get("/arroseur/off", (req, res) => {
+  client.publish("arroseur", JSON.stringify({ status: "end" }));
+  res.send("The arroseur is off </br> <a href='http://localhost:3000/arroseur/on'>Turn on the light</a>");
+
+})
 
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:3000");
 })
