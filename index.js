da06734dc467e7ece8a6e39897c07d2ecadcf0e1
Thomas Louis Nardou <pk88yte@eduvaud.ch>
2024-12-12 16:00:58 +0100

    refactor(app): change app port

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ const client = mqtt.connect("mqtt://blue.section-inf.ch:1883", {
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<a href='http://localhost:3000/arroseur/on'>Turn on the light</a>  ||  <a href='http://localhost:3000/arroseur/off'>Turn off the light</a>");
+  res.send("<a href='http://localhost:8080/arroseur/on'>Turn on the light</a>  ||  <a href='http://localhost:8080/arroseur/off'>Turn off the light</a>");
 });
 
 app.get("/arroseur/on", (req, res) => {
   client.publish("arroseur", ConvertToJson({ status: "start" }));
-  res.send("The light is on </br> <a href='http://localhost:3000/arroseur/off'>Turn off the light</a>");
+  res.send("The light is on </br> <a href='http://localhost:8080/arroseur/off'>Turn off the light</a>");
 
 });
 
 app.get("/arroseur/off", (req, res) => {
   client.publish("arroseur", ConvertToJson({ status: "end" }));
-  res.send("The light is off </br> <a href='http://localhost:3000/arroseur/on'>Turn on the light</a>");
+  res.send("The light is off </br> <a href='http://localhost:8080/arroseur/on'>Turn on the light</a>");
 
 })
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
 })
